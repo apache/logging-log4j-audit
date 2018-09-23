16fa48335b4ff7f06102689f237b5f4e7d398f79
Andrei Ivanov <andrei.ivanov@arnia.ro>
2018-09-23 15:00:00 +0300

    [LOG4J2-2429] don't cascade on method invocations

--- log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/LoginTest.java
+++ log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/LoginTest.java
@@ -20,9 +20,22 @@ public class LoginTest extends BaseEventTest {
 
         event.logEvent();
 
+        AuditExceptionHandler exceptionHandler = (message, ex) -> {
+
+        };
+        event.setAuditExceptionHandler(exceptionHandler);
+        event.logEvent();
+
         List<String> msgs = app.getMessages();
         assertNotNull("No messages", msgs);
-        assertTrue("No messages", msgs.size() == 2);
+        assertTrue("No messages", msgs.size() == 3);
+
+        String msg = msgs.get(1);
+        assertTrue("No completionStatus", msg.contains("completionStatus=\"Success\""));
+
+        msg = msgs.get(2);
+        assertTrue("auditExceptionHandler should not be present in the context", !msg.contains("auditExceptionHandler=\""));
+        msgs.forEach(System.out::println);
     }
 
     @Test
