da385005f631386b72255910db9579b84064b96c
Andrei Ivanov <andrei.ivanov@arnia.ro>
2018-09-23 15:04:36 +0300

    [LOG4J2-2429] cleanup assertions

--- log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/LoginTest.java
+++ log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/LoginTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class LoginTest extends BaseEventTest {
     @Test
@@ -28,13 +27,13 @@ public class LoginTest extends BaseEventTest {
 
         List<String> msgs = app.getMessages();
         assertNotNull("No messages", msgs);
-        assertTrue("No messages", msgs.size() == 3);
+        assertEquals("No messages", 3, msgs.size());
 
         String msg = msgs.get(1);
         assertTrue("No completionStatus", msg.contains("completionStatus=\"Success\""));
 
         msg = msgs.get(2);
-        assertTrue("auditExceptionHandler should not be present in the context", !msg.contains("auditExceptionHandler=\""));
+        assertFalse("auditExceptionHandler should not be present in the context", msg.contains("auditExceptionHandler=\""));
         msgs.forEach(System.out::println);
     }
 
