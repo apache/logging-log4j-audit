a8fa433c1a6a83fa36f1a3379ada30e8df98e0fc
Andrei Ivanov <andrei.ivanov@arnia.ro>
2019-04-02 15:25:17 +0300

    LOG4J2-2580

--- log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/catalog/CatalogManagerTest.java
+++ log4j-audit/log4j-audit-api/src/test/java/org/apache/logging/log4j/audit/catalog/CatalogManagerTest.java
@@ -19,17 +19,24 @@ package org.apache.logging.log4j.audit.catalog;
 import org.apache.logging.log4j.catalog.api.Event;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
  *
  */
 public class CatalogManagerTest {
-
     @Test
     public void testCatalog() throws Exception {
         CatalogManager manager = new CatalogManagerImpl(new StringCatalogReader());
         Event event = manager.getEvent("transfer");
         assertNotNull("No transfer event", event);
+        assertEquals("{\"name\" : \"transfer\", \"displayName\" : \"Transfer\", \"description\" : \"Transfer between accounts\", " +
+                             "\"attributes\" : [{\"name\" : \"toAccount\", \"required\" : true}, {\"name\" : \"fromAccount\", \"required\" : true}, {\"name\" : \"amount\", \"required\" : true}]}",
+                     event.toString());
+
+        Event login = manager.getEvent("login");
+        assertNotNull("No login event", login);
+        assertEquals("{\"name\" : \"login\", \"displayName\" : \"Login\", \"description\" : \"User Login\", \"attributes\" : []}", login.toString());
     }
 }
