ce4d6f9c9a16472ab2f3b786210c7e64305ce8c7
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-10-31 15:26:36 +0100

    Fix breaking change in SpringFox 2.10.x

--- log4j-catalog/log4j-catalog-editor/src/main/java/org/apache/logging/log4j/catalog/config/SwaggerConfig.java
+++ log4j-catalog/log4j-catalog-editor/src/main/java/org/apache/logging/log4j/catalog/config/SwaggerConfig.java
@@ -28,13 +28,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * This will configure Swagger to produce an API for all of our REST endpoints.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @EnableWebMvc
 public class SwaggerConfig {
     @Bean
